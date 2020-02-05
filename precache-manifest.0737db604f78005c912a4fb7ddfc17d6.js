3a4037aeb4544453ee5aae630ae10ab5438c22b2
manuj5533 <manuj5533@gmail.com>
2020-02-05 16:58:42 +0530

    Updates

---
+++ precache-manifest.0737db604f78005c912a4fb7ddfc17d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65fc74066439af2b1bb1904245f68b7e",
+    "url": "/reactdev/index.html"
+  },
+  {
+    "revision": "f62b8a51f81a2f85935c",
+    "url": "/reactdev/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "faf14ce2221964c1bbcd",
+    "url": "/reactdev/static/js/2.d1b85aeb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/reactdev/static/js/2.d1b85aeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f62b8a51f81a2f85935c",
+    "url": "/reactdev/static/js/main.4934eca0.chunk.js"
+  },
+  {
+    "revision": "ecca8daa574dc60226d2",
+    "url": "/reactdev/static/js/runtime-main.a71fbe86.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactdev/static/media/logo.5d5d9eef.svg"
+  }
+]);
